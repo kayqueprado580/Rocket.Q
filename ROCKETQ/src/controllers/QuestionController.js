050be2b6269020d85c688986f006151bf9a970eb
Kayque Prado Silva <46896994+kayqueprado580@users.noreply.github.com>
2021-06-26 16:50:20 -0300

    Projeto v1.0

--- ROCKETQ/src/controllers/QuestionController.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    index(req, res) {
-        const roomId = req.params.room
-        const questionId = req.params.question
-        const action = req.params.action
-        const password = req.body.password
-
-    }
-}
